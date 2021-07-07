b0d6acd737ed6ec319c397b753cb8c45b65250bd
faizansaghir <dbz.rashidsaghir@gmail.com>
2021-07-07 13:23:20 +0530

    Made content.js and filter.js independant

--- contentAndFilter/content.js
+++ contentAndFilter/content.js
@@ -26,29 +26,13 @@ function reInitializeProducts(images,descriptions,prices){
         buildProduct(images[i],descriptions[i],prices[i])
     }
 }
-if(document.getElementById("middle")){
-    let productDescription=["Beach Image","Vacation Photo","Pool Pic","Random Click","Shore Photo","Ocean Beauty","Food Cravings"];
-    let productPrice=["Rs. 200","Rs.150","Rs.300","Rs.50","Rs.100","Rs.150","Rs.75"];
-    let productImage=["1.0.png","2.0.png","3.0.png","4.0.png","5.0.png","6.0.png","7.0.jpg"];
-    populateProducts(productImage,productDescription,productPrice);
-}
-else{
-    document.body.innerHTML+=`
-        <div class="container-fluid" id="middle">
-            <div class="row">
-                <div class="filterArea col-sm-12 col-md-3">
-                    Filter Area
-                </div>
-                <div class="contentArea col-sm-12 col-md-9">
-                    <div class="row" id="products">
-                        
-                    </div>
-                </div>
-            </div>
+document.getElementById("contentAndFilter").innerHTML+=`
+    <div class="contentArea col-sm-12 col-md-9">
+        <div class="row" id="products">
         </div>
-    `;
+    </div>
+`;
     let productDescription=["Beach Image","Vacation Photo","Pool Pic","Random Click","Shore Photo","Ocean Beauty","Food Cravings"];
     let productPrice=["Rs. 200","Rs.150","Rs.300","Rs.50","Rs.100","Rs.150","Rs.75"];
     let productImage=["1.0.png","2.0.png","3.0.png","4.0.png","5.0.png","6.0.png","7.0.jpg"];
     populateProducts(productImage,productDescription,productPrice);
-}
--- contentAndFilter/filter.js
+++ contentAndFilter/filter.js
@@ -1,14 +1,5 @@
-document.body.innerHTML+=`
-    <div class="container-fluid" id="middle">
-        <div class="row">
-            <div class="filterArea col-sm-12 col-md-3">
-                Filter Area
-            </div>
-            <div class="contentArea col-sm-12 col-md-9">
-                <div class="row" id="products">
-                    
-                </div>
-            </div>
-        </div>
+document.getElementById("contentAndFilter").innerHTML+=`
+    <div class="filterArea col-sm-12 col-md-3">
+        Filter Area
     </div>
 `;
