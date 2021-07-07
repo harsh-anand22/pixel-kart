2b0b3d3d1477ea2bce1367db6021a5bada10528b
faizansaghir <dbz.rashidsaghir@gmail.com>
2021-07-07 13:15:08 +0530

    Generalized home.html and added code to render content and filter via JS

--- contentAndFilter/content.js
+++ contentAndFilter/content.js
@@ -15,26 +15,40 @@ function buildProduct(image,description,price){
     template=template.replace("{}",price);
     document.getElementById("products").innerHTML+=template;
 }
-function populateProducts(){
-    for(let i=0;i<productImage.length;i++){
-        buildProduct(productImage[i],productDescription[i],productPrice[i])
+function populateProducts(images,descriptions,prices){
+    for(let i=0;i<images.length;i++){
+        buildProduct(images[i],descriptions[i],prices[i])
     }
 }
-function reInitializeProducts(){
+function reInitializeProducts(images,descriptions,prices){
     document.getElementById("products").innerHTML='';
-    for(let i=0;i<productImage.length;i++){
-        buildProduct(productImage[i],productDescription[i],productPrice[i])
+    for(let i=0;i<images.length;i++){
+        buildProduct(images[i],descriptions[i],prices[i])
     }
 }
-
-
-let productDescription=["Beach Image","Vacation Photo","Pool Pic","Random Click","Shore Photo","Ocean Beauty","Food Cravings"];
-let productPrice=["Rs. 200","Rs.150","Rs.300","Rs.50","Rs.100","Rs.150","Rs.75"];
-let productImage=["1.0.png","2.0.png","3.0.png","4.0.png","5.0.png","6.0.png","7.0.jpg"];
-
-populateProducts();
-
-document.getElementById("middle").innerHTML+=`  
-`;
-
-
+if(document.getElementById("middle")){
+    let productDescription=["Beach Image","Vacation Photo","Pool Pic","Random Click","Shore Photo","Ocean Beauty","Food Cravings"];
+    let productPrice=["Rs. 200","Rs.150","Rs.300","Rs.50","Rs.100","Rs.150","Rs.75"];
+    let productImage=["1.0.png","2.0.png","3.0.png","4.0.png","5.0.png","6.0.png","7.0.jpg"];
+    populateProducts(productImage,productDescription,productPrice);
+}
+else{
+    document.body.innerHTML+=`
+        <div class="container-fluid" id="middle">
+            <div class="row">
+                <div class="filterArea col-sm-12 col-md-3">
+                    Filter Area
+                </div>
+                <div class="contentArea col-sm-12 col-md-9">
+                    <div class="row" id="products">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    let productDescription=["Beach Image","Vacation Photo","Pool Pic","Random Click","Shore Photo","Ocean Beauty","Food Cravings"];
+    let productPrice=["Rs. 200","Rs.150","Rs.300","Rs.50","Rs.100","Rs.150","Rs.75"];
+    let productImage=["1.0.png","2.0.png","3.0.png","4.0.png","5.0.png","6.0.png","7.0.jpg"];
+    populateProducts(productImage,productDescription,productPrice);
+}
--- contentAndFilter/filter.js
+++ contentAndFilter/filter.js
@@ -1,2 +1,14 @@
-document.getElementById("middle").innerHTML+=`
+document.body.innerHTML+=`
+    <div class="container-fluid" id="middle">
+        <div class="row">
+            <div class="filterArea col-sm-12 col-md-3">
+                Filter Area
+            </div>
+            <div class="contentArea col-sm-12 col-md-9">
+                <div class="row" id="products">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 `;
